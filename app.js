7f13650315eb0376bd2fe8c075692cee012a447e
Chika Shigemoto <Chika.Shigemoto1@ibm.com>
2023-04-19 00:24:11 +0900

    最低限DB操作追加。

--- app.js
+++ app.js
@@ -2,58 +2,109 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.type('html').send(html));
+var { Client } = require("pg");
+
+require("dotenv").config();
+
+const env = process.env;
+console.log(env.DATABASE_NAME);
+
+const client = new Client({
+  user: env.USER_NAME,
+  host: env.HOST,
+  database: env.DATABASE_NAME,
+  password: env.PASSWORD,
+  post: env.POSTGRE_PORT,
+});
+
+app.get("/", (req, res) => {
+  console.log("req.body: ");
+  console.log(req.body);
+
+  (async () => {
+    try {
+      const connection = await client.connect();
+
+      await client.query(`DROP DATABASE IF EXISTS ${env.DATABASE_NAME};`);
+      await client.query(`CREATE DATABASE ${env.DATABASE_NAME};`);
+
+      await client.query(
+        `CREATE TABLE ${env.TABLE_NAME}(id SERIAL PRIMARY KEY, value VARCHAR NOT NULL);`
+      );
+
+      const value = "val1";
+
+      await insertRecords(connection, value);
+
+      console.log("insert終了");
+    } catch (e) {
+      console.log("e: " + e);
+    } finally {
+      client.end();
+    }
+  })();
+});
+
+app.get("/select", (req, res) => {
+  (async () => {
+    try {
+      const selectResult = await selectRecords(connection);
+
+      console.log("selectResult: " + selectResult);
+      console.log("select終了");
+    } catch (e) {
+      console.log("e: " + e);
+    } finally {
+      client.end();
+    }
+  })();
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
+async function insertRecords(connection, value) {
+  return await new Promise(function(resolve, reject) {
+    connection.query(
+      `INSERT IGNORE INTO ${env.TABLE_NAME} (value) VALUES ?;`,
+      value,
+      (err, rows, fields) => {
+        if (err) {
+          console.log("err: ");
+          console.log(err);
+          console.log("err.code: ");
+          console.log(err.code);
 
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Hello from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
-        });
-      }, 500);
-    </script>
-    <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
+          reject(err);
+        }
+
+        console.log("rows: ");
+        console.log(rows);
+
+        resolve();
       }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
+    );
+  });
+}
+
+async function selectRecords(connection) {
+  return await new Promise(function(resolve, reject) {
+    connection.query(
+      `SELECT * from ${env.TABLE_NAME};`,
+      (err, rows, fields) => {
+        if (err) {
+          console.log("err: ");
+          console.log(err);
+          console.log("err.code: ");
+          console.log(err.code);
+
+          reject(err);
+        }
+
+        console.log("rows: ");
+        console.log(rows);
+
+        resolve();
       }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
-  </head>
-  <body>
-    <section>
-      Hello from Render!
-    </section>
-  </body>
-</html>
-`
+    );
+  });
+}
